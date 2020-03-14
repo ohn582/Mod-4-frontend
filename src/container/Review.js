912ebbbd567ee153b7c8243fd59f545217a324d6
ohn582 <49073115+ohn582@users.noreply.github.com>
2020-03-14 19:01:27 -0400

    Update Review.js

--- src/container/Review.js
+++ src/container/Review.js
@@ -7,7 +7,7 @@ import { fetchReviews } from '../actions/index'
 
 class Review extends Component {
 
-    componentDidMount() { // this is where all of our fetch happens
+    componentDidMount() { 
         this.props.fetchReviews()
     }
 
@@ -41,7 +41,3 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps, { fetchReviews })(Review);
 
-
-//mapStateToProps() method is executed with each change to the store's state.
-
-//In this lesson we saw that the connect() function is used for us to connect our Redux part of the application to the React part of the application (we'll see even more of this later). We also see that whatever function we pass as the first argument to that connect() function is called each time there is a change of state, and has access to the entire store's state. The connect() function then takes the return value from the mapStateToProps() function and adds that return value to the props of the component that is passed through in the last parentheses. We call that component a connected component, because that component is connected to the store.
